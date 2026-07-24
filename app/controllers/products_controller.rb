fd5d13aa12eecaff326cb8e6d636b5668d0bc56a
kiranpreet867 <kkaur127@academic.rrc.ca>
2026-07-24 01:31:02 -0500

    Add keyword and category product search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,27 @@
 class ProductsController < ApplicationController
   def index
+    @categories = Category.order(:name)
     @products = Product.includes(:category).order(:name)
+
+    if params[:search].present?
+      keyword = "%#{Product.sanitize_sql_like(params[:search])}%"
+
+      @products = @products.where(
+        "products.name ILIKE :keyword OR products.description ILIKE :keyword",
+        keyword: keyword
+      )
+    end
+
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
+
+    case params[:filter]
+    when "sale"
+      @products = @products.where(on_sale: true)
+    when "new"
+      @products = @products.where(created_at: 3.days.ago..Time.current)
+    end
   end
 
   def show
