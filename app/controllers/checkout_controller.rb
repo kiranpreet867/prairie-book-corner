4ab82901847cac1160cecde09576dbf65bf5c6ff
kiranpreet867 <kkaur127@academic.rrc.ca>
2026-07-29 23:29:33 -0500

    Add breadcrumb navigation

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -5,7 +5,6 @@ class CheckoutController < ApplicationController
   def create
     session[:cart] = {}
 
-    redirect_to root_path,
-                notice: "Thank you! Your order has been placed."
+    redirect_to root_path, notice: "Thank you! Your order has been placed."
   end
 end
